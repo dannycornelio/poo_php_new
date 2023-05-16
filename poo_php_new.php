44015b92871b3ab97eea327953f9fab54ba3c13e
dannycornelio <licdannycornelio@gmail.com>
2023-05-15 21:28:15 -0600

    ultima actualizacion 9:28

--- poo_php_new.php
+++ poo_php_new.php
@@ -1,52 +1,69 @@
 <?php
-// Se define la clase abstracta "ObraLiteraria"
-abstract class ObraLiteraria{
-    protected $titulo;
-    public $autor;
-    private static $genero = "Ficción";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+//aqui creo la clase Comins
+abstract class Comins{
+//aqui el modificador de acceso protegido
+    protected $genero;
+//aqui el modificador de acceso publico
+    public $titulo;
+//aqui el modificador de acceso privado
+    private $autor;
 
-    public function __construct($titulo, $autor){
+    public static $estado = "Emision";
+
+//aqui el metodo __construct se ejecuta al ejercer la instancia 
+    public function __construct($genero, $titulo, $autor){
+// se le das valores a los atributos segun los parametros en el metodo construct
+        $this->genero = $genero;
         $this->titulo = $titulo;
         $this->autor = $autor;
     }
-
-    final private static function getGenero(){
-        echo "El género de la obra es " . self::$genero;
+//definicion de metodo
+    final public static function getEstado(){
+        echo "La obra esta en estado " . self::$estado;
     }
 
     public function getInfo() : void{
-        echo "La obra literaria se titula \"$this->titulo\", y es del autor $this->autor.";
+        echo "El titulo es $this->titulo, y de genero $this->genero";
+    }
+    public function getAutor(): void{
+        echo "El autor de la obra es:  $this->autor ";
     }
 
-    abstract protected function estado();
 }
-
-// Se define la clase hija "Manga" que extiende de "ObraLiteraria"
-class Manga extends ObraLiteraria{
-    protected $volumenes;
-
-    public function __construct($titulo, $autor, $volumenes = 0){
-        $this->volumenes = $volumenes;
-        parent::__construct($titulo, $autor);
+//aqui la clase manga es la hija y la Comins es el padre de quien se heredan las caracteristicas
+class Manga extends Comins{
+    public $volumen;
+    //metodo
+    public function __construct($volumen = 0, $genero = 0, $titulo = 0, $autor = 0){
+        $this->volumen = $volumen;
+        parent::__construct($genero, $titulo, $autor);
     }
-
+   //metodo
     public function getInfo() : void{
-        echo "El manga \"$this->titulo\" es del autor $this->autor, tiene $this->volumenes volumenes, y el género es: " . parent::$genero;
-    }
-
-    protected function estado(){
-        return "En emisión";
+        echo "El Manga es titulado $this->titulo, de genero $this->genero, su volumen $this->volumen, y esta en estado " . parent::$estado;
     }
 }
 
-//$obraLiteraria = new ObraLiteraria("La sombra del viento", "Carlos Ruiz Zafón");
-$obraLiteraria->getInfo();
-echo "<br>";
-Manga::getGenero();
-echo "<br>";
-$mangaOnePiece = new Manga("One Piece", "Eiichiro Oda", 100);
-$mangaOnePiece->getInfo();
-echo "<br>";
-echo "El estado del manga \"$mangaOnePiece->titulo\" es: " . $mangaOnePiece->estado();
+//aqui la clase Manhwa hereda de comins
+class Manhwa extends Comins{
+//metodo 
+    public function getInfo() : void{
+        echo "El Manhwa es titulado:  $this->titulo, de genero $this->genero, y esta en estado " . parent::$estado;
+    }
+}
 
+//instancia de la clase Manga
+$manga1 = new Manga( 15,"Shonen", "One Piece", "Ichiro Oda");
+$manga1->getInfo();
+echo "<br>";
+$manga1->getAutor();
+echo "<br>";
+//instancia de la clase Manhwa
+$manhwa1 = new Manhwa("Shonen", "Solo Leveling", "Cornelio");
+$manhwa1->getInfo();
+echo"<br>";
+$manhwa1->getAutor();
 ?>
